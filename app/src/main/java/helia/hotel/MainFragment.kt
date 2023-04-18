c13edb14a5a9bfdf87eafa1882a1f60a0c7cc586
asilb <asilbekqayumovu@gmail.com>
2023-04-18 17:33:57 +0500

    new1

--- app/src/main/java/helia/hotel/MainFragment.kt
+++ app/src/main/java/helia/hotel/MainFragment.kt
@@ -1,24 +1,22 @@
- package helia.hotel
+package helia.hotel
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import helia.hotel.databinding.FragmentLogInBinding
 
- // TODO: Rename parameter arguments, choose names that match
+// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LogInFragment.newInstance] factory method to
+ * Use the [MainFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LogInFragment : Fragment() {
+class MainFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,10 +33,8 @@ class LogInFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       val binding = FragmentLogInBinding.inflate(inflater, container, false)
-        binding.SignUp.setOnClickListener {
-            findNavController().navigate(R.id.lo)
-        }
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     companion object {
@@ -48,12 +44,12 @@ class LogInFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment LogInFragment.
+         * @return A new instance of fragment MainFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            LogInFragment().apply {
+            MainFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
